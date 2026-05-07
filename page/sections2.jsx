646ca24940b66a6f6cef3e22eb0124f0bd6dbe93
박요한 <bag-yohan@bag-yohan-ui-MacBookAir.local>
2026-05-07 15:31:48 +0900

    Bust browser cache for clinical02 with v=smooth

--- page/sections2.jsx
+++ page/sections2.jsx
@@ -513,7 +513,7 @@ const Clinical02Section = () => {
       </div>
 
       {/* 3개 카드 */}
-      <img src="https://cdn.jsdelivr.net/gh/contactocreator/tae743-detail-assets@main/clinical02.gif" alt="clinical02-bars" style={{ width: "100%", height: "auto", display: "block" }} />
+      <img src="https://cdn.jsdelivr.net/gh/contactocreator/tae743-detail-assets@main/clinical02.gif?v=smooth" alt="clinical02-bars" style={{ width: "100%", height: "auto", display: "block" }} />
     </section>
   );
 };
